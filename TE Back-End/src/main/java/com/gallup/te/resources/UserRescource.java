58525fe2cebddc5e178db1f35c54f65b0598aaef
Jcbgid <jgid261@hotmail.com>
2016-02-13 12:25:17 -0600

    Resource update

--- TE Back-End/src/main/java/com/gallup/te/resources/UserRescource.java
+++ TE Back-End/src/main/java/com/gallup/te/resources/UserRescource.java
@@ -4,11 +4,15 @@ package com.gallup.te.resources;
 import java.sql.SQLException;
 
 import javax.ws.rs.Consumes;
+import javax.ws.rs.DELETE;
 import javax.ws.rs.GET;
 import javax.ws.rs.POST;
+import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.QueryParam;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
 
 import com.gallup.te.DataSourceManager;
 import com.gallup.te.model.User;
@@ -19,25 +23,19 @@ public class UserRescource {
 
 	@GET
 	@Produces("application/json")
-	public User getUser(@QueryParam("param") String uId, @QueryParam("searchBy") String filterBy) {
+	public User getUser(@QueryParam("param") String param) {
 		try {
-			if (filterBy.equals("param")) {
-				User send = getDao().queryForId(uId);
+				User send = getDao().queryForId(param);
 				if (send == null) {
 					return null;
 				} else {
 					return send;
 				}
-			} else if (filterBy.equals("lName")) {
-
+			}catch(SQLException e){
+				e.printStackTrace();
+				return null;
 			}
-
-		} catch (SQLException e) {
-			e.printStackTrace();
-			return null;
 		}
-		return null;
-	}
 
 	@POST
 	@Produces("text/plain")
@@ -57,10 +55,13 @@ public class UserRescource {
 				} else {
 					// If creating a user with duplicate studentID
 					if (getDao().queryForEq("username", u.getUsername()).size() > 1) {
+						Response.ok(json,MediaType.APPLICATION_JSON);
+						Response.notModified("User already exists);
 						return "invalid username";
 						// Creating a new user with unique studentID
 					} else {
 						return "created";
+				
 				}
 				}
 					}
@@ -69,6 +70,44 @@ public class UserRescource {
 			e.printStackTrace();
 			return "Error";
 		}
+	}
+
+	@PUT
+	@Produces("text/plain")
+	@Consumes("application/json")
+	public String updateUser(User u) {
+		try {
+			int rowsChanged = getDao().update(u);
+			if (rowsChanged == 0) {
+				return "No entries found for User " + u.getUserID();
+			} else if (rowsChanged > 1) {
+				return "Error With Updating User " + u.getUserID();
+			} else if (rowsChanged == 1) {
+				return "Entry updated for User " + u.getUserID();
+			}
+		} catch (SQLException e) {
+			return "Error";
+		}
+		return null;
+	}
+
+	@DELETE
+	@Produces("test/plain")
+	public String deleteUser(@QueryParam("uID") String uID) {
+		try {
+			int rowsChanged = getDao().deleteById(uID);
+			if (rowsChanged == 1) {
+				return "User " + uID + " removed";
+			} else if (rowsChanged == 0) {
+				return "User " + uID + " not found";
+			} else {
+				return "Issue with Delete";
+			}
+		} catch (SQLException e) {
+			e.printStackTrace();
+			return "Error";
+		}
+
 	}
 
 	private Dao<User, String> getDao() {
