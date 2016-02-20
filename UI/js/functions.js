b703f6bc22e30b5438ef4298674dc189f9287aa0
ezhang16 <ezhang16@dashonline.org>
2016-02-20 12:49:52 -0600

    Update functions.js

--- UI/js/functions.js
+++ UI/js/functions.js
@@ -1,7 +1,17 @@
 $( document ).ready(function() {
     $.ajax({url: "https://raw.githubusercontent.com/GETHIP-Classroom/get-hip-project-2016-tutor-exchange/master/UI/users.json", success: function(result){
         result = JSON.parse(result);
-        console.log(result);
+        var binary_array = (""+result.user.schedule).split("")
+        var boolean = [];
+        $.each(binary_array, function(index,value){
+        if(value == "0"){
+            boolean.push(false)
+        }
+        if(value == "1"){
+            boolean.push(true)
+        }
+        });
+        result.user.schedule = boolean;
         console.log(result.user.schedule);
     }});
 });
