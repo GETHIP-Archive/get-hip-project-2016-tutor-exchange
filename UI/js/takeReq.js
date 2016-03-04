a488a5adc9dc7a17111321b5ca54d45381ea8eb7
Jcbgid <jgid261@hotmail.com>
2016-03-03 21:28:56 -0600

    update

--- UI/js/takeReq.js
+++ UI/js/takeReq.js
@@ -1,17 +1,18 @@
-var btn;
-var return;
-$('#btnsub').click(function(){
+var input;
+var response;
+$('#takebtn').click(function(){
+        input = $('#enterrequestid').val();
        $.ajax({
         url: "http://localhost:9998/api/request?param=8422&type=tutorID", 
         method: 'GET',
         success: function(result){
-            return = result.request;
-            return.tutorID = 8422;
+            response = result.request;
+            response.tutorID = 8422;
     $.ajax({
         url: "http://localhost:9998/api/request", 
         method: 'PUT',
          contentType: 'application/json',
-        data: JSON.stringify(request),
+        data: JSON.stringify(response),
         success: function(result){
             console.log('success');
              window.location.reload();
