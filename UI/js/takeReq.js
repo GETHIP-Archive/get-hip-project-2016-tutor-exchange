77026999f96766c119863ff67566c0e73a006e4e
Jcbgid <jgid261@hotmail.com>
2016-03-03 21:13:26 -0600

    Update

--- UI/js/takeReq.js
+++ UI/js/takeReq.js
@@ -1,44 +1,13 @@
-var user;
-$( document ).ready(function() {
-    $.ajax({
-        url: "http://localhost:9998/api/request?param=1&type=status", 
+var btn;
+var return;
+$('#btnsub').click(function(){
+       $.ajax({
+        url: "http://localhost:9998/api/request?param=8422&type=tutorID", 
         method: 'GET',
         success: function(result){
-       
-        user = result;
-        var binary_array = (""+result.user.schedule).split("");
-        var boolean = [];
-        $.each(binary_array, function(index,value){
-        if(value == "0"){
-            boolean.push(false)
-        }
-        if(value == "1"){
-            boolean.push(true)
-        }
-        });
-        result.user.schedule = boolean;
-        console.log(result.user.schedule);
-        $.each(boolean, function(index, value){
-            $('#cb'+ index).prop('checked', value);
-        });
-    }});
-});
+            return = result.request;
+        
 
-$('#btnsub').click(function(){
-        var booleanarray = [];
-        var binaryString = '';
-    for(var i = 0; i < 10; i++){
-        booleanarray.push($('#cb'+ i).is(':checked'));
-    }
-    $.each(booleanarray, function(index,value){
-        if(value){
-             binaryString = binaryString + 1;
-        }else{
-            binaryString = binaryString + 0;
-        }
-    });
-    user.user.schedule = binaryString;
-    console.log(binaryString);
     $.ajax({
         url: "http://localhost:9998/api/user", 
         method: 'PUT',
@@ -48,7 +17,8 @@ $('#btnsub').click(function(){
             console.log('success');
              window.location.reload();
         }
-    });
+              });
+        });
 });
 
 
